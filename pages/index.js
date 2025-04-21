b51c901fdd1c9ba844ed145298e387d0bff07dc2
godavebay <davebay@hotmail.com>
2025-04-21 12:33:07 -0400

    cluster fix 8

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,8 @@
-// Trigger commit for Vercel redeploy
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to LuxeCampUSA</h1>
+      <p>Your gateway to the most exclusive campgrounds and glamping retreats across the USA.</p>
+    </div>
+  );
+}
